26ebabbdda516c258d608c983a87a779911b0e41
Fransisco Wijaya <bidipeppercrap@outlook.com>
2020-08-16 22:08:32 +0700

    package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "spelltest",
+    version = "0.0.1",
+    author = "bidipeppercrap",
+    author_email = "bidipeppercrap@outlook.com",
+    description = "SPELL TEST mechanics",
+    packages=setuptools.find_packages(),
+    python_requires = ">=3.8",
+)
